5751b24452c4d98226a458771bf2dd3d8436b3cc
kienp2901 <kienp2901@gmail.com>
2022-07-20 14:28:53 +0700

    change

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 const port = 3000
 
-app.get('/trang-chu', (req, res) => {
+app.use(morgan('combined'))
+
+app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
