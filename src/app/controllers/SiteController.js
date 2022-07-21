a8db49bb0c848c8778a377a0c41dd5377998294f
kienp2901 <kienp2901@gmail.com>
2022-07-21 17:47:21 +0700

    change

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,13 +1,19 @@
+var db = require("../models/db");
 class SiteController {
-    //[GET] /
-    index(req, res) {
-        res.render('home');
-    }
+  //[GET] /
+  index(req, res) {
+    let sql = `SELECT * FROM course`;
+    db.query(sql, function (err, data) {
+      let listData = data;
+      res.render("home", { listData });
+      //res.json(listData);
+    });
+  }
 
-    //[GET] /search
-    search(req, res) {
-        res.render('search');
-    }
+  //[GET] /search
+  search(req, res) {
+    res.render("search");
+  }
 }
 
 module.exports = new SiteController();
--- src/index.js
+++ src/index.js
@@ -5,10 +5,7 @@ const handlebars = require('express-handlebars');
 const app = express();
 const port = 3000;
 const route = require('./routes');
-const db = require('./config/db');
 
-//Connect to DB
-db.connect();
 
 app.use(express.static(path.join(__dirname, 'public')));
 
